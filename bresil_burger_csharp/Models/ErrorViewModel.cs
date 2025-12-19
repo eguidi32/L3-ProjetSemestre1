2ad906425607224d5327216d8c762cd440331838
Mahamat Nour BRAHIM <mahamatnourbrahim235@gmail.com>
2025-12-19 09:10:32 +0000

    Suppression du dossier bresil_burger_csharp et du fichier bresil_burger_csharp.sln

--- bresil_burger_csharp/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace bresil_burger_csharp.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
